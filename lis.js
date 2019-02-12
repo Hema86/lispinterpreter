5a314bf54496b91fcedb9e1bedd74981ad60265c
Hema86 <hemakempur@gmail.com>
2019-02-12 08:49:38 +0530

    added repl

--- lis.js
+++ lis.js
@@ -1,5 +1,5 @@
 'use strict'
-let env = {
+let ENV = {
   '+': (valArr) => {
     return valArr.reduce((a, b) => {
       return a * 1 + b * 1
@@ -57,7 +57,7 @@ function evaluater (input) {
   if (input.startsWith('(')) {
     input = input.slice(1)
     let fn = identifier(input)
-    if (fn in env) return sExpression(input)
+    if (fn in ENV) return sExpression(input)
     else return specialForm(input)
   }
   if (strParser(input) !== null) return strParser(input)
@@ -71,24 +71,23 @@ function sExpression (input) {
     if (input.startsWith('(')) {
       input = input.slice(1)
       let result = sExpression(input)
-      args.push(result)
-      input = input.slice(input.indexOf(')') + 1)
-      input = spaceParser(input)
+      args.push(result[0])
+      input = spaceParser(result[1])
     } else {
       let val = identifier(input)
-      if (val in env) {
-        args.push(env[val])
+      if (val in ENV) {
+        args.push(ENV[val])
         input = input.slice(val.length)
         input = spaceParser(input)
       } else {
         let result = evaluater(input)
-        if (result !== null) { args.push(result) }
-        // fn = identifier(input)
-        input = input.slice(input.indexOf(' '))
+        if (result !== null) { args.push(result[0]) }
+        result = result[0].toString()
+        input = input.slice(result.length)
         input = spaceParser(input)
       }
     }
-  } return env[fn](args)
+  } return [ENV[fn](args), spaceParser(input.slice(1))]
 }
 function specialForm (input) {
   if (input.startsWith('if')) return parserIf(input)
@@ -100,69 +99,30 @@ function specialForm (input) {
 function parserIf (input) {
   input = input.slice(2)
   input = spaceParser(input)
-  let checkArgs = input.substring(1)
-  let fn = identifier(checkArgs)
-  checkArgs = checkArgs.slice(fn.length, checkArgs.indexOf(')') + 1)
-  checkArgs = spaceParser(checkArgs)
-  let arg
-  if (checkArgs.startsWith('(')) {
-    arg = evaluater(checkArgs)
-    input = input.replace(checkArgs, arg)
-  }
-  checkArgs = input.substring(1)
-  fn = identifier(checkArgs)
-  checkArgs = checkArgs.slice(fn.length)
-  checkArgs = spaceParser(checkArgs)
-  checkArgs = checkArgs.slice(input.indexOf(' ') + 1, checkArgs.indexOf(')') + 1)
-  checkArgs = spaceParser(checkArgs)
-  if (checkArgs.startsWith('(')) {
-    arg = evaluater(checkArgs)
-    input = input.replace(checkArgs, arg)
-  }
-  let test = input.substring(0, input.indexOf(')') + 1)
-  input = input.slice(input.indexOf(')') + 1)
-  input = spaceParser(input)
-  let conseq
-  if (input.startsWith('(')) {
-    conseq = input.substring(0, input.indexOf(')') + 1)
-    input = input.slice(input.indexOf(')') + 1)
-  } else {
-    conseq = input.substring(0, input.indexOf(' '))
-    input = input.slice(input.indexOf(' ') + 1)
-  }
-  input = spaceParser(input)
-  let alt
-  if (input.startsWith('(')) {
-    alt = input.substring(0, input.indexOf(')') + 1)
-    input = input.slice(input.indexOf(')') + 1)
-  } else {
-    alt = input.substring(0, input.indexOf(')'))
-    input = input.slice(input.indexOf(' ') + 1)
-  }
-  input = spaceParser(input)
-  if (evaluater(test)) return evaluater(conseq)
-  else return evaluater(alt)
+  let result = evaluater(input)
+  let conseq = evaluater(result[1])
+  if (conseq[1].startsWith(')')) return 'Invalid expression'
+  let alt = evaluater(conseq[1])
+  if (result[0] === true) return conseq[0]
+  else return alt[0]
 }
 function parserDef (input) {
   input = input.slice(6)
   input = spaceParser(input)
-  let key
-  if (input.startsWith('(')) {
-    key = evaluater(input)
-    input = input.slice(input.indexOf(')') + 1)
+  let result = evaluater(input)
+  let key = result[0]
+  input = spaceParser(result[1])
+  if (input.startsWith('(lambda')) {
+    input = input.slice(7)
+    // let val2 = Object.create(null)
+    ENV[key] = {}
+    ENV[key]['env'] = {}
+    ENV[key]['env']['parent'] = ENV
+    ENV[key]['env']['args'] = {}
+    ENV[key]['env']['args']['x'] = null
   } else {
-    key = input.substring(0, input.indexOf(' '))
-    input = input.slice(key.length)
-  }
-  input = spaceParser(input)
-  let val
-  if (input.startsWith('(')) {
-    val = input.substring(0, input.length - 1)
-    env[key] = val
-  } else {
-    val = input.substring(0, input.indexOf(')'))
-    val = evaluater(val)
-    env[key] = val
+    let val = evaluater()
+    ENV[key] = val[0]
   }
   return 'prop added'
 }
@@ -170,9 +130,10 @@ function parserBegin (input) {
   input = input.slice(6)
   let defCheck = input.substring(0, input.indexOf(')') + 1)
   evaluater(defCheck)
-  input = input.slice(defCheck.length)
+  input = input.slice(defCheck.length + 1)
   input = spaceParser(input)
-  return evaluater(input)
+  let result = evaluater(input)
+  return result[0]
 }
 function parserQuote (input) {
   if (input.startsWith('\'')) {
@@ -187,8 +148,8 @@ function parserQuote (input) {
 function parserLambda (input) {
   input = input.slice(6)
   input = spaceParser(input)
-  let symbol = input.substring(0, input.indexOf(')') + 1)
-  input = input.slice(symbol.length)
+  let args = evaluater(input)
+  input = input.slice(args.length)
   input = spaceParser(input)
   return evaluater(input)
 }
@@ -200,42 +161,26 @@ function identifier (str) {
 function numberParser (str) {
   let regEx = /^-?(0|[\d1-9]\d*)(\.\d+)?(?:[Ee][+-]?\d+)?/
   let value = str.match(regEx)
-  if (value !== null) return value[0] * 1
+  if (value !== null) return [value[0] * 1, str.slice(value[0].length, str.length)]
   else return null
 }
 function strParser (str) {
   let regEx = /^[A-Za-z]+/
-  let result = regEx.exec(str)
-  return result[0]
+  let value = str.match(regEx)
+  if (value !== null) return [value[0], str.slice(value[0].length, str.length)]
+  else return null
 }
 function spaceParser (str) {
   let regex = /^\s+/
   str = str.replace(regex, '')
   return str
 }
-// let result = evaluater('(define x 8)')
-// console.log(result)
-// let res = evaluater('(if (> 10 20) (+ 1 1) (+ 3 3))')
-// console.log(res)
-// let res1 = evaluater('(begin (define r 10) (* pi (* r r)))')
-// console.log(res1)
-// let res2 = evaluater('(+ (+ x x) (* x x))')
-// console.log(res2)
-// let res3 = evaluater('(define (define g 4) 4)')
-// console.log(res3)
-// let res4 = evaluater('(quote (+ 1 2))')
-// console.log(res4)
-// let res5 = evaluater('(lambda (r) (* pi (* r r)))')
-// console.log(res5)
-// let res6 = evaluater('(define r (* 6 7))')
-// console.log(res6)
-// let res7 = evaluater('(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))')
-// console.log(res7)
-// let res8 = evaluater('(fact 10)')
-// console.log(res8)
-// let res9 = evaluater('(define r 10)')
-// console.log(res9)
-// let res10 = evaluater('(* pi (* r r))')
-// console.log(res10)
-let res11 = evaluater('(if (> (* 11 11) 120) (* 7 6) oops)')
-console.log(res11)
+// // function repl () {
+//   var stdin = process.openStdin()
+//   stdin.addListener('data', function (input) {
+//     let res = evaluater(input.toString().trim(), null)
+//     if (res !== null) console.log(JSON.stringify(res))
+//   })
+// }
+// repl()
+console.log(evaluater('(define square (lambda (x) (* x x)))'))
